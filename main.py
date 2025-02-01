779ecbbed8783579397f99e9cef83027535b3b86
hayter bordon <hayterbordon.d@gmail.com>
2025-02-01 01:55:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -87,17 +87,9 @@ def obtener_fragmento_grande(video_url):
         }],
     }
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-<<<<<<< HEAD
         info_dict = ydl.extract_info(video_url, download=True)
         audio_file_path = ydl.prepare_filename(info_dict).replace('.webm', '.mp3').replace('.m4a', '.mp3')
         return open(audio_file_path, 'rb')
-=======
-        info_dict = ydl.extract_info(video_url, download=False)
-        audio_url = info_dict['url']
-        response = requests.get(audio_url)
-        audio_stream = BytesIO(response.content)
-        return audio_stream
->>>>>>> 818c130 (Guardando cambios antes del rebase)
 
 def eliminar_archivo_temporal(filepath):
     try:
